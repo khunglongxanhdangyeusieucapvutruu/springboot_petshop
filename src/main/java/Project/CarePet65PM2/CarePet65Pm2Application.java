8df81262829f9c697c07d3cf7922b1ae77922117
Uoptsuu <102488800+Uoptsuu@users.noreply.github.com>
2023-04-07 15:40:08 +0700

    commit

---
+++ src/main/java/Project/CarePet65PM2/CarePet65Pm2Application.java
@@ -0,0 +1,13 @@
+package Project.CarePet65PM2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarePet65Pm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarePet65Pm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Project/CarePet65PM2/CarePet65Pm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.CarePet65PM2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarePet65Pm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
