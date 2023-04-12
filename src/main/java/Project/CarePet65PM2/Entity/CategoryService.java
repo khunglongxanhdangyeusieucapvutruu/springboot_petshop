7d9f9ef4614c590ca522e1da8bcfb3e3dbd5ce66
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:05:29 +0700

    Update CategoryService.java

--- src/main/java/Project/CarePet65PM2/Entity/CategoryService.java
+++ src/main/java/Project/CarePet65PM2/Entity/CategoryService.java
@@ -1,4 +1,22 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category_service")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name="cs_id")
+    private int Id;
+    @Column( name="cs_name")
+    private String Name;
 }
