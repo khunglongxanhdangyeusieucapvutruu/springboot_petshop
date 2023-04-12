73c56432ee425df0444a3327cdcc5d1241624251
Uoptsuu <102488800+Uoptsuu@users.noreply.github.com>
2023-04-12 23:29:37 +0700

    commit

--- src/main/java/Project/CarePet65PM2/Entity/Admin.java
+++ src/main/java/Project/CarePet65PM2/Entity/Admin.java
@@ -1,4 +1,36 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "admin") //ten bang trong database
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
+    @Id
+    @Column(name="ad_id")  //ten truong trong database
+    private int Id;
+    @Column(name="ad_name")
+    private String Name;
+    @Column(name="personid")
+    private String PersonId;
+    @Column(name="gender")
+    private boolean Gender;
+    @Column(name="birthday")
+    private LocalDate Birthday;
+    @Column(name="password")
+    private String Password;
+    @Column(name="role")
+    private int Role;
 }
