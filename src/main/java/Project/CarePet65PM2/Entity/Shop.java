2c62a78f22ef99e218b3ed8f1894d5c01c802546
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:12:33 +0700

    Update Shop.java

--- src/main/java/Project/CarePet65PM2/Entity/Shop.java
+++ src/main/java/Project/CarePet65PM2/Entity/Shop.java
@@ -1,4 +1,32 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "shop_info")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Shop {
+   @Column(name="shop_name")
+    private String Name;
+    @Column(name="address")
+    private String Address;
+    @Column(name="shop_phone")
+    private String Phone;
+    @Column(name="shop_description")
+    private String Description;
+    @Column(name="shop_facebook")
+    private String Facebook;
+    @Column(name="shop_website")
+    private String Website;
+    @Column(name="shop_banner")
+    private String Banner;
+    @Column(name="shop_logo")
+    private String Logo;
 }
