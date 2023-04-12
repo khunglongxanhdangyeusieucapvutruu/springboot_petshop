0b940f58d1db3a48d57011340032a4cbd2fcc105
Uoptsuu <102488800+Uoptsuu@users.noreply.github.com>
2023-04-12 23:36:49 +0700

    commit

--- src/main/java/Project/CarePet65PM2/Entity/Admin.java
+++ src/main/java/Project/CarePet65PM2/Entity/Admin.java
@@ -1,13 +1,7 @@
 package Project.CarePet65PM2.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -19,6 +13,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Admin {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ad_id")  //ten truong trong database
     private int Id;
     @Column(name="ad_name")
