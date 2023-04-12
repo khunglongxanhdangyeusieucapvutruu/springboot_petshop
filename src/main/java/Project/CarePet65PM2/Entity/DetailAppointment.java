6f118e5c90723eb191844aa72b04d9eda96368d1
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:06:29 +0700

    Update DetailAppointment.java

--- src/main/java/Project/CarePet65PM2/Entity/DetailAppointment.java
+++ src/main/java/Project/CarePet65PM2/Entity/DetailAppointment.java
@@ -1,4 +1,24 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "detail_appointment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetailAppointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="apm_id")
+    private int AppointmentId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="sv_id")
+    private int ServiceId;
 }
