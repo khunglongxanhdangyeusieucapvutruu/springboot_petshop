02855f6fb7d2c90c682a1cc8fd96310f8cb60a0c
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:08:40 +0700

    Update Feedback.java

--- src/main/java/Project/CarePet65PM2/Entity/Feedback.java
+++ src/main/java/Project/CarePet65PM2/Entity/Feedback.java
@@ -1,4 +1,26 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "feedback")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="fb_id")
+    private int Id;
+    @Column(name="fb_content")
+    private String Content;
+    @Column(name="fb_time")
+    private String Time;
+    @Column(name="ctm_id")
+    private String CustomerId;
 }
