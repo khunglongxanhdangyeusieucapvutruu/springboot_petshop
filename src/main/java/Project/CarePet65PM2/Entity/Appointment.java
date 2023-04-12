0485f6ee30191f618d4ecba901044eadeb6f32c4
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:02:58 +0700

    Update Appointment.java

--- src/main/java/Project/CarePet65PM2/Entity/Appointment.java
+++ src/main/java/Project/CarePet65PM2/Entity/Appointment.java
@@ -1,4 +1,31 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "appointment") //ten bang trong database
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="apm_id")  //ten truong trong database
+    private int Id;
+    @Column( name ="apm_time")
+    private LocalDate Time;
+    @Column( name="apm_status")
+    private  int Status;
+    @Column( name ="ctm_id")
+    private int CustomerId;
 }
