307b04af76b62f020cd181b3709674c6e2d4ca0e
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:07:42 +0700

    Update DetailService.java

--- src/main/java/Project/CarePet65PM2/Entity/DetailService.java
+++ src/main/java/Project/CarePet65PM2/Entity/DetailService.java
@@ -1,4 +1,26 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "detail_service")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetailService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="sv_id")
+    private int ServiceId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="mtr_id")
+    private int MaterialId;
+    @Column(name="quantity")
+    private int Quantity;
 }
