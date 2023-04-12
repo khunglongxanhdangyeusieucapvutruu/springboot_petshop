804346402d9084dd6d500e40b451952417fa6c06
khunglongxanhdangyeusieucapvutruu <129540609+khunglongxanhdangyeusieucapvutruu@users.noreply.github.com>
2023-04-13 02:04:56 +0700

    Update CategoryNews.java

--- src/main/java/Project/CarePet65PM2/Entity/CategoryNews.java
+++ src/main/java/Project/CarePet65PM2/Entity/CategoryNews.java
@@ -1,4 +1,22 @@
 package Project.CarePet65PM2.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category_news")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryNews {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name="cn_id")
+    private int Id;
+    @Column( name ="cn_name")
+    private String Name;
 }
